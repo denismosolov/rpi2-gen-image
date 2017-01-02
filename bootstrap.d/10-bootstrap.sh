d8e4ff8198297afcd625e80b37b75cb694b1e8b9
Denis Mosolov <denismosolov@gmail.com>
2017-01-02 16:48:46 +0300

    Suppress posisble prompts

--- bootstrap.d/10-bootstrap.sh
+++ bootstrap.d/10-bootstrap.sh
@@ -41,7 +41,7 @@ else
   cp -r "$CHROOT_SOURCE"/* "${R}"/
   chroot "${R}" bash -c 'echo nameserver 8.8.4.4 > /etc/resolv.conf'
   chroot "${R}"/ apt-get update
-  chroot "${R}"/ apt-get install -y `echo $APT_INCLUDES | sed 's/,/ /g'`
+  DEBIAN_FRONTEND=noninteractive chroot "${R}"/ apt-get -y -qq -f -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install `echo $APT_INCLUDES | sed 's/,/ /g'`
 fi
 
 # Mount required filesystems
