6c5107e5ee9b18bb89021ab3937d67871d98f98a
Denis Mosolov <denismosolov@gmail.com>
2017-03-18 20:18:09 +0300

    Suppress possible prompts.

--- bootstrap.d/11-apt.sh
+++ bootstrap.d/11-apt.sh
@@ -46,6 +46,6 @@ if [ -d packages ] ; then
     chroot_exec dpkg --unpack /tmp/$(basename $package)
   done
 fi
-chroot_exec apt-get -qq -y -f install
+chroot_exec apt-get -qq -y -f -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install
 
 chroot_exec apt-get -qq -y check
